cd1b61d63868adcc54d4c3e9513bcad70878b452
Patrick Williams <patrick@bittorrent.com>
2012-07-03 15:54:33 -0700

    floating away better

--- floataway.jquery.js
+++ floataway.jquery.js
@@ -2,14 +2,14 @@
     $.fn.floatAway = function() {  
         return this.each(function() {
                 var that = $(this);
-        	that.animate({
-	        		'margin-left': '40px',
-	        		'margin-top': '-60px',
-	        		'opacity': 0.3
-	        	}, 1500, 'linear', function() {
-        			that.remove();
-        		}
-        	);
+            that.animate({
+                    'margin-right': '-40px',
+                    'margin-top': '-60px',
+                    'opacity': 0.0
+                }, 1500, 'linear', function() {
+                    that.remove();
+                }
+            );
         });
     };
 })( jQuery );
